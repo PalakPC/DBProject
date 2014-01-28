98c15c90112844688e51f49b18ebc7575b4023b1
Siddharth Choudhary <siddharthchoudhary@gatech.edu>
2014-01-28 08:39:02 -0500

    server initial

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ var connection = mysql.createConnection({
 			database: "dbprojectdb",
 		});
 
+//Not sure if this works correctly. TODO
+function handleDisconnect(connection) {
+	connection.on('error', function(err){
+				console.log('\nReconnecting...  Lost connection: ' + err.stack);
+				connection.destroy();
+
+				connection = mysql.createConnection(connection.config);
+				handleDisconnect(connection);
+				connection.connect();
+			});
+}
+
+handleDisconnect(connection);
+connection.connect();
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/login_page', function(req, res){
+		res.render('login_page.html');
+		});
